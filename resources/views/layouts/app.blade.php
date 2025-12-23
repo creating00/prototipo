1d62272c62ed3c19782e13ef5e6fba50a83d19e4
creating00 <creatingsr@gmail.com>
2025-12-23 16:25:37 -0300

    error adminLTE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<!--begin::Head-->
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <title>@yield('page-title', config('app.name', 'Laravel'))</title>
 
     <meta name="supported-color-schemes" content="light dark" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/js/adminlte-components.js'
+    ])
 
-    <title>@yield('page-title', 'App')</title>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/adminlte-components.js'])
     @stack('styles')
 </head>
 
 <body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
-    <!--begin::App Wrapper-->
+    <!-- App Wrapper -->
     <div class="app-wrapper">
-        <!--begin::Header-->
-        <x-adminlte.header />
-        <!--end::Header-->
-        <!--begin::Sidebar-->
-        <x-adminlte.sidebar />
-        <!--end::Sidebar-->
-        <!--begin::App Main-->
-        <x-admin-lte.app-main>
-            @yield('content')
-        </x-admin-lte.app-main>
 
-        <!--end::App Main-->
-        <!--begin::Footer-->
-        <x-adminlte.footer />
-        <!--end::Footer-->
+        <!-- Header -->
+        @include('adminlte.partials.header')
+
+        <!-- Sidebar -->
+        @include('adminlte.partials.sidebar')
+
+        <!-- Main Content -->
+        <main class="app-main">
+            @yield('content')
+        </main>
+
+        <!-- Footer -->
+        @include('adminlte.partials.footer')
+
     </div>
+
     @stack('scripts')
 </body>
-<!--end::Body-->
-
 </html>
